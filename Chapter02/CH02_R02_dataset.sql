c903d43765d7b93890f6d49968d305de2ea70fe0
JP Bonard <68299538+jpbonard@users.noreply.github.com>
2024-10-17 23:36:47 -0700

    Chapter2

---
+++ Chapter02/CH02_R02_dataset.sql
@@ -0,0 +1,2 @@
+SELECT id, name, address, city, state, country, zipcode, birthday, age, gender, marital_status, dependents, phone_number, email, preferences, birthcountry, annualincome
+	FROM public.ch01_customers;
